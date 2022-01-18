03b8d694921229e8ab3a989740d4dc0f66618cdc
Aaron Torres <51248787+aarontorres0@users.noreply.github.com>
2022-01-17 22:54:48 -0600

    created components

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  LayersControl,
-  LayerGroup,
-} from "react-leaflet";
+
 import "./App.css";
+import Header from "./Header";
+import Map from "./Map";
 
 function App() {
-  const center = [51.505, -0.09];
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={7} scrollWheelZoom={true}>
-      <LayersControl position="topright">
-        <LayersControl.BaseLayer checked name="OpenStreetMap.Mapnik">
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </LayersControl.BaseLayer>
-        <LayersControl.BaseLayer name="OpenStreetMap.BlackAndWhite">
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
-          />
-        </LayersControl.BaseLayer>
-        <LayersControl.Overlay checked name="Marker with popup">
-          <Marker position={center}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </LayersControl.Overlay>
-      </LayersControl>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
+    <div>
+      <Header />
+      <Map />
+    </div>
   );
 }
 
